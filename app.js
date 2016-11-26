aa2d7ff7f94754292d56f9108d7d5ac0799202aa
panforrest <guoqianp@gmail.com>
2016-11-26 15:48:50 -0500

    start oct20 setup post page

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 // var users = require('./routes/users');
 var profilesearch = require('./routes/profilesearch');
+var post = require('./routes/post');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+app.set('view engine', 'mustache');
+app.engine('mustache', require('hogan-middleware').__express);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 // app.use('/users', users);
 app.use('/profilesearch', profilesearch);
+app.use('/post', post);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
