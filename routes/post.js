2a0e3232b9abea098693e9b7a6fdad512676cbec
panforrest <guoqianp@gmail.com>
2016-11-26 17:30:43 -0500

    dynamic user code render json on page

--- routes/post.js
+++ routes/post.js
@@ -6,14 +6,15 @@ router.get('/:code', function(req, res, next){
     var code = req.params.code
     console.log(code)
 
-    url = 'https://api.instagram.com/oembed/?url=http://instagram.com/p/BBNtIVxras8/'
+    url = 'https://api.instagram.com/oembed/?url=http://instagram.com/p/'+code
     superagent
     .get(url)
     .query(null)
     .set('Accept', 'application/json')
     .end(function(err, response){
         console.log(JSON.stringify(response.body))
-        res.render('post', null)
+        res.json(response.body)
+        // res.render('post', response.body)
     })
 
 	
