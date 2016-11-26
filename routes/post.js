c29eb3056c5d6498309aacfbe4796201ce41bb61
panforrest <guoqianp@gmail.com>
2016-11-26 17:21:25 -0500

    3000/post/BNQW8QLja67 console logs json at terminal

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,22 @@
 var express = require('express')
 var router = express.Router()
+var superagent = require('superagent')
 
 router.get('/:code', function(req, res, next){
-	res.render('post', null)
+    var code = req.params.code
+    console.log(code)
+
+    url = 'https://api.instagram.com/oembed/?url=http://instagram.com/p/BBNtIVxras8/'
+    superagent
+    .get(url)
+    .query(null)
+    .set('Accept', 'application/json')
+    .end(function(err, response){
+        console.log(JSON.stringify(response.body))
+        res.render('post', null)
+    })
+
+	
 })
 
 module.exports = router
